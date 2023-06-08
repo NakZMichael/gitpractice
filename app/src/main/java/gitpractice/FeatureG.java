acbb28804497c4972bdb340779efc736760b79ff
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2023-06-09 02:10:06 +0900

    機能Gを修正

--- app/src/main/java/gitpractice/FeatureG.java
+++ app/src/main/java/gitpractice/FeatureG.java
@@ -2,7 +2,7 @@ package gitpractice;
 
 import java.util.stream.IntStream;
 
-public class FeatureC {
+public class FeatureG {
   public void fizzbuzzFrom0To100() {
     IntStream.range(0, 100).forEach(i -> Utils.fizzbuzz(i));
   }
