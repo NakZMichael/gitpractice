1b541130ae20cfd1ba5ed3f248f5e0fdf59c4e7e
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2023-06-09 02:49:13 +0900

    共通機能を実装

---
+++ app/src/main/java/gitpractice/Utils.java
@@ -0,0 +1,15 @@
+package gitpractice;
+
+public class Utils {
+  public static String fizzbuzz(int num) {
+    if(num % 15 == 0 ){
+      return "fizzbuzz" ;
+    }else if(num % 3 == 0 ){
+      return "fizz";
+    }else if(num %5 == 0){
+      return "buzz";
+    }else{
+      return String.valueOf(num);
+    }
+  }
+}
